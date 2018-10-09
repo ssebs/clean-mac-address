d4d9809a7e317b64225208c42a37e5af8e133bf5
ssafari <ssafari@parc.com>
2018-10-09 08:03:59 -0700

    processed mac

--- clean-mac.py
+++ clean-mac.py
@@ -5,7 +5,35 @@ from tkinter import messagebox
 
 def proccess_mac(mac_in):
     mac_out = ""
+    split_char = ''
+    mac_ary = []
+    final_mac_ary = []
+    mac_in = mac_in.strip()
 
+    # check seperator
+    if ":" in mac_in:
+        split_char = ':'
+    elif "-" in mac_in:
+        split_char = '-'
+    else:
+        messagebox.showerror("Error", "Enter a MAC Address that contains a : or -")
+
+    # create mac addr array 
+    mac_ary = mac_in.split(split_char)
+
+    if len(mac_ary) != 6:
+        messagebox.showerror("Error", "Make sure the MAC is valid!")
+
+    # remove leading 0 + lowercase
+    for x in mac_ary:
+        tmp = x.lower()
+        if tmp.startswith("0"):
+            tmp = tmp[1]
+        final_mac_ary.append(tmp)
+
+    for x in final_mac_ary:
+        mac_out += x + ":"
+    mac_out = mac_out[:-1]
     
     return mac_out
 # end process_mac
